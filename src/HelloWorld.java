d774d5d718750d5577d9e379126f46872171d70f
hosonomika <hosonomika@localhost>
2026-05-01 16:13:46 +0900

    1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,18 @@
-public class HelloWorld {
+public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("HelloWorld");
+        
+        for (int i = 1; i <= 100; i++) {
+            
+            if (i % 15 == 0) {
+                System.out.println("FizzBuzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+            
+        }
     }
 }
